d7edfc0adc8f81d32a8181e6a9406ebe16e5d3ce
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-05-23 13:52:41 -0500

    style: update global styles and improve footer and home page layout
    Update the global CSS to adjust neutral color and add footer shadow. Enhance the Footer component with styling and copyright text. Refactor the home page layout to include a background image and mission/vision sections.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,50 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div 
-      className="
-        min-h-screen 
-        p-8 pb-20 gap-16 sm:p-20 
-      ">
-      
+    <div
+      className=" 
+        flex flex-col
+      "
+    >
+      <div 
+          className="
+          h-96 md:min-h-screen 
+          relative
+        "
+      >
+        <Image src="/images/main-image.png" fill alt="background" />
+      </div>
+      <div
+        className="
+          flex flex-col md:flex-row justify-around items-center
+          gap-10
+          my-16 lg:my-24 mx-20 lg:mx-60
+        "
+      >
+        <div 
+          className="
+            flex flex-col items-center justify-center gap-5 
+            text-center 
+            border border-neutral-3 rounded-[12px]
+            py-4 px-6
+          "
+        >
+          <p className="text-[36px] font-[600]">MISIÓN</p>
+          <p className="text-[16px]">Impulsar startups de impacto social mediante el desarrollo de soluciones escalables, sostenibles e innovadoras, que respondan a problemáticas reales de comunidades vulnerables y promuevan el bienestar colectivo.</p>
+        </div>
+        <div 
+          className="
+            flex flex-col items-center justify-center gap-5 
+            text-center 
+            border border-neutral-3 rounded-[12px]
+            py-4 px-6
+          "
+        >
+          <p className="text-[36px] font-[600]">VISIÓN</p>
+          <p className="text-[16px]">Ser líderes en innovación social en Latinoamérica para el año 2030, reconocidos por generar ecosistemas emprendedores resilientes, inclusivos y colaborativos que transformen positivamente la vida de millones de personas.</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="text-center text-[12px] py-3 shadow-footer">
+      © 2025 Digenio. Todos los derechos reservados.
+    </footer>
   );
 };
